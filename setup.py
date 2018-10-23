7534206afb496058a464fa5a26599a91d96f315f
Jeongmin Byun <jmbyun91@gmail.com>
2018-10-23 12:55:55 +0900

    add readme and setup files

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="urobot",
+    version="0.0.1",
+    author="Jeongmin Byun",
+    author_email="jmbyun91@gmail.com",
+    description="Environment for controlling a robot on the grid world for learning to program in python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/jmbyun/urobots",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
